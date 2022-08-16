474bf07a7cc1db3c12590b7f29817e2e762688bd
UIM GIT-Master <ui.manufaktur@gmail.com>
2022-08-16 10:29:52 +0200

    Working on base

--- source/uim/html/elements/base.d
+++ source/uim/html/elements/base.d
@@ -7,10 +7,10 @@ import uim.html;
 class DH5Base : DH5Obj {
 	mixin(H5This!("base"));
 
-  // href = URL - Specifies the base URL for all relative URLs in the page
+  // Attribute href - Specifies the base URL for all relative URLs in the page
   mixin(MyAttribute!("href"));   
 
-  // ATtribute target - A keyword or author-defined name of the default browsing context to show the results of navigation
+  // Attribute target - A keyword or author-defined name of the default browsing context to show the results of navigation
   mixin(MyAttribute!("target")); // _blank, _parent, _self o. _top - Specifies the default target for all hyperlinks and forms in the page
 }
 mixin(H5Short!"Base");
